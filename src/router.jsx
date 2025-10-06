6fc6d3416a338940abe6a8545bee0a75e9da0947
Dhayananth B <dhayananthtn@gmail.com>
2025-10-06 15:00:05 +0530

    .

--- src/router.jsx
+++ src/router.jsx
@@ -1,10 +1,10 @@
-import { createBrowserRouter } from "react-router-dom"
+import { createHashRouter } from "react-router-dom"
 import App from "./App.jsx"
 import About from "./components/About.jsx"
 import Contact from "./components/Contact.jsx"
 import Body from "./components/Body.jsx"
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     {
         path: "/", element: <App />, children: [
         {
